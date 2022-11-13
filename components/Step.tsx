4c5812f9fdf308a33face34ccb80e701d463dd88
Azat Faezov <60698950+farscince@users.noreply.github.com>
2022-11-13 09:58:15 +0600

    fonts were fixed

--- components/Step.tsx
+++ components/Step.tsx
@@ -11,7 +11,7 @@ const Step: FC<IStepProps> = ({
 }) => (
   <li className='flex w-full items-center mb-12'>
     <div className='relative w-16 h-16 bg-modal rounded-tr-3xl rounded-bl-3xl'>
-      <div className='absolute top-0 right-0 font-bold text-6xl w-full h-full mr-[-50%] mt-[-50%]'>
+      <div className='absolute top-0 right-0 font-bold text-7xl w-full h-full mr-[-50%] mt-[-40%]'>
         {index}
       </div>
     </div>
